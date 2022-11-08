cbb9e025e6189a2dd0e22042bc30f2c088d0f3f6
ziigzagz <zigzagzaczax@gmail.com>
2022-11-08 12:40:03 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 // import logo from './logo.svg';
 import './App.css'
-import  Index  from './views/Index'
-import  Info  from './views/Info'
+import Index from './views/Index'
+import Info from './views/Info'
 
 function App() {
   return (
@@ -10,9 +10,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/info" element={<Info />} />
+        <Route></Route>
       </Routes>
     </div>
   )
 }
 
 export default App
+
+
--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 function Index() {
-    return (
-        <div>
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet libero dolorem error enim officia ad adipisci eius, recusandae quod molestias necessitatibus in reiciendis architecto maiores nesciunt. Optio fuga veniam unde.
-        </div>
-    );
+  return <div>มอนิ่งงงง</div>
 }
 
-export default Index;
+export default Index
