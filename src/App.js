107a4538652d3eb4757c453ad08f1e6250ec4861
ziigzagz <zigzagzaczax@gmail.com>
2022-11-08 11:40:49 +0700

    add info page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 // import logo from './logo.svg';
 import './App.css'
 import  Index  from './views/Index'
+import  Info  from './views/Info'
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/info" element={<Info />} />
       </Routes>
     </div>
   )
---
+++ src/views/Info.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Info(props) {
+    return (
+        <div>
+            this page has been updated
+            now showing Info page
+        </div>
+    );
+}
+
+export default Info;
