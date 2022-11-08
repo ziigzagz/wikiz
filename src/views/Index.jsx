69e979d6fb8f3eb918e9a66fcd380404ac8496c7
ziigzagz <zigzagzaczax@gmail.com>
2022-11-08 11:35:23 +0700

    Update Index.jsx

--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Index() {
     return (
         <div>
-    
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet libero dolorem error enim officia ad adipisci eius, recusandae quod molestias necessitatibus in reiciendis architecto maiores nesciunt. Optio fuga veniam unde.
         </div>
     );
 }
